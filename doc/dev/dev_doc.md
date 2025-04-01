bd13a0b0c5363d314f09f107d6b76a532be39960
Ónodi András Péter <onodiandraspeter@ktch.hu>
2025-04-01 13:45:21 +0200

    Kezdés

---
+++ doc/dev/dev_doc.md
@@ -0,0 +1,21 @@
+# Fejlesztői dokumentáció
+
+## Az alkalmazás célja
+
+Rendezés bemutatása egy egyszerű Java programmal.
+
+# Használt eszközök
+
+A Java nyelven írt program Visual Studio Code-dal készült. A projekt a "No build tools" projektgenerálóval készült.
+
+Windows képmetsző a képernyőképekhez. 
+
+A forráskódok az src könyvtárban találhatók.
+
+## Az App osztály
+
+Az App osztály az alkalmazás belépési pontja. Csak az alkalmazás inditására használjuk.
+
+##  A Store osztály
+
+A Store osztály segítségével beolvassuk a .csv kiterjesztésü fájl tartalmát.
---
+++ src/Store.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Store {
+    public ArrayList<Employee> readFile() {
+        try {
+            return tryReadFile();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+        ArrayList<Employee> empList = new ArrayList<>();
+        String fileName = "dolgozok_002_rendbetett.csv";
+        File file = new File(fileName);
+
+        try(Scanner sc = new Scanner(file)) {
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+        }
+
+
+        return empList;
+    }
+}
